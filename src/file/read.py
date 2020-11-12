4c1e6a2e91e98e1ad0be7afe5415125b96ff0126
Daria <dasha21a@icloud.com>
2020-11-12 20:42:35 +0300

    Add a logic file for read.py.

--- src/file/read.py
+++ src/file/read.py
@@ -2,35 +2,23 @@
 # read() все строки
 # readlines() все строки перекидываются в лист
 
-list2 = list([])
-list3 = open("27-A_demo.txt", "r").read().split(" ")
+list1 = open("27-A_demo.txt", "r").read().split(" ")
 
-for i in list3:
-    if(len(i) == 0):
-        list3.remove(i)
+for i in list1:
+    if len(i) == 0:
+        list1.remove(i)
 
-list4 = []
-for i in list3:
-    if(i.__contains__("\n")):
-        k =  i.replace("\n", " ")
-        list4.append(k)
+list2 = []
+for i in list1:
+    if i.__contains__("\n"):
+        list2.append(i.replace("\n", " ").split(" "))
 
-
-list5 = []
-
-for i in list4:
-    if(i.__contains__(" ")):
-        o = i.split(" ")
-        list5.append(o)
-
-list6 = []
-for i in list5:
-    if(len(i) > 1):
+list3 = []
+for i in list2:
+    if len(i) > 1:
         for j in i:
-            list6.append(j)
+            list3.append(j)
+            if len(j) == 0:
+                list3.remove(j)
 
-for i in list6:
-    if(len(i) == 0):
-        list6.remove(i)
-
-print(list6)
+print(list3)
